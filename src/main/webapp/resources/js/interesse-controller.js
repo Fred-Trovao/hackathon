94efe3b25fbd45c65fd2889bb0fb3001052477cd
Lucas <lucasfigueiredoweb@gmail.com>
2017-02-19 04:01:07 -0300

    adicionado cadastro de interesse

--- src/main/webapp/resources/js/interesse-controller.js
+++ src/main/webapp/resources/js/interesse-controller.js
@@ -1,9 +1,11 @@
-angular.module('inspinia').controller('InteresseController', function($rootScope, $scope, AuthSharedService) {
+angular.module('inspinia').controller('InteresseController', function($rootScope, $scope, AuthSharedService, $http) {
     $scope.interesse = {};
     $scope.interesse.tipoDoInteresse = 0;
     
 	$scope.salvarInformacoes = function(){
-		console.log($scope.interesse);
+		$http.post("/interesses/salvarInteresse",$scope.interesse).success(function(response) {
+	        console.log("Interesse cadastrado com sucesso!");
+	    });
     	
     }
 });
