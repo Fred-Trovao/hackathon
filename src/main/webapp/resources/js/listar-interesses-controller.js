608f2d59f57257743582159efb03a57f7444872d
José Carlos Lacerda <josecarloslacerda@MacBook-Pro-de-Jose.local>
2017-02-19 04:56:41 -0300

    implementacao do listar

--- src/main/webapp/resources/js/listar-interesses-controller.js
+++ src/main/webapp/resources/js/listar-interesses-controller.js
@@ -1,5 +1,17 @@
 angular.module('inspinia').controller('ListarInteressesController', function($rootScope, $scope, AuthSharedService, $http) {
     
+	this.listarInteresses = function(){
+		
+		$http({
+			method: 'GET',
+			url: '/listar-interesses'
+				
+		}).then(function sucesso(response){
+			
+		});
+	}
+	
+	listarInteresses();
 	
 	$scope.listaInteresse = [
 		{
@@ -25,6 +37,37 @@ angular.module('inspinia').controller('ListarInteressesController', function($ro
 			valorDoRepasse : 3400.51,
 			valorDaParcela : 500.00,
 			quantidadeDeParcelas : 5,
+			usuario : {
+				endereco : {
+					cidade : 'João Pessoa',
+					estado : 'PB',
+				},
+				nome : 'Mark Johnson',
+				email : 'mark@msn.com',
+				telefone : '83 88888-8888',
+			}
+		},
+		
+	];
+	
+	$scope.listaInteressados = [
+		{
+			id : 1,
+			valorDoInteresse : 2000.00,
+			usuario : {
+				endereco : {
+					cidade : 'João Pessoa',
+					estado : 'PB',
+				},
+				nome : 'Monica Smith',
+				email : 'monica@msn.com',
+				telefone : '83 99999-9999',
+			}
+		},
+		{
+			id : 2,
+			valorDoInteresse : 34234.00,
+			valorDoRepasse : 3400.51,
 			usuario : {
 				endereco : {
 					cidade : 'João Pessoa',
